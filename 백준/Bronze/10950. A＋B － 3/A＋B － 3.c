e768a84250298748d0ed897c2012e7a7c14a0abe
gaxunsxo <137467879+gaxunsxo@users.noreply.github.com>
2024-01-26 14:11:50 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int t;//num of test cases
+    scanf("%d", &t);
+
+    int a, b;
+
+    for(int i=0; i<t; i++){
+        scanf("%d %d\n", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+
+    return 0;
+}

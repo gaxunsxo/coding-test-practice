48a5ff998d287f8c871a80147bd3d444aaa45dc8
gaxunsxo <137467879+gaxunsxo@users.noreply.github.com>
2024-01-30 21:41:08 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
@@ -0,0 +1,32 @@
+// 다른 사람 풀이
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int main()
+{
+    int n, m;
+    scanf("%d %d\n", &n, &m);
+
+    int arr[n+1];
+
+    for(int i=0 ;i<=n; i++){
+        arr[i] = i;
+    }
+
+    int i, j;
+    while(m--){ //for문 말고 이런 방법이 있군
+        scanf("%d %d", &i, &j);
+
+        for(int x = 0; x <= (j-i)/2; x++){
+            swap(&arr[i+x], &arr[j-x]);
+        }
+    }
+
+    for(int i=1; i<=n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

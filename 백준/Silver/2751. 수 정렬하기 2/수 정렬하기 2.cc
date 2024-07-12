bb253e4cd0bb171be8caf888ba02f910774d4bf0
gaxunsxo <137467879+gaxunsxo@users.noreply.github.com>
2024-07-12 14:28:00 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 356 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,26 @@
+/**
+ * 2751: 수 정렬하기 2
+ * N개의 수를 오름차순으로 정렬하는 프로그램
+ * !!!왜 시간초과??? -> 입출력 문제?
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    std::vector<int> numbers(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    std::sort(numbers.begin(), numbers.end());
+
+    for(int i = 0; i < n; i++) {
+        printf("%d\n", numbers[i]);
+    }
+
+    return 0;
+}

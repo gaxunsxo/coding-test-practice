1dd8879951778cd4487d3195d7fbc50b8abae9ec
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-08-08 20:52:57 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString();
+
+let score = Number(input);
+
+if (score >= 90 && score <= 100) 
+  console.log("A");
+else if (score >= 80 && score <= 89)
+  console.log("B");
+else if (score >= 70 && score <= 79)
+  console.log("C");
+else if (score >= 60 && score <= 69)
+  console.log("D");
+else if (score >= 0 && score <= 59)
+  console.log("F");

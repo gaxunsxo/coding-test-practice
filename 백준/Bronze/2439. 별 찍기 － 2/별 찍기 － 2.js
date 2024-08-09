dfdfe0c481845681617814d9d8b66c14dfe04e05
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-08-09 16:01:19 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 120 ms, Memory: 9636 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString();
+
+let n = Number(input);
+let answer = "";
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < n - i; j++) {
+    answer += " ";
+  }
+  for (let j = 0; j < i; j++) {
+    answer += "*";
+  }
+  answer += "\n";
+}
+console.log(answer);

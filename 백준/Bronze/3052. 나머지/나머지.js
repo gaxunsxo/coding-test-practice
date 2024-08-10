b4b9cbe105193967a87bdcaeeaaf3c69dfb832c6
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-08-10 09:24:27 +0900

    [Bronze II] Title: 나머지, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -3,8 +3,11 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");;
 
 // 3052: 나머지
 arr = [];
+
+// 각 입력을 42로 나눈 나머지를 저장 
 for (let i = 0; i < 10; i++) 
   arr.push(input[i] % 42);
 
+// 집합의 원소 개수를 출력 
 let mySet = new Set(arr);
 console.log(mySet.size);

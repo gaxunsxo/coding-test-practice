a8583d7121a75df8d44518387a3b206e9ed5e1b5
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-10-01 13:46:24 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 9980 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,20 +1,23 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");;
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
+// 2750. 수 정렬하기
 let n = Number(input[0]);
 let arr = [];
-for (let i = 1; i <= n; i++) {
-  arr.push(Number(input[i]));
+for (let i = 0; i < n; i++) {
+  arr.push(Number(input[i + 1]));
 }
 
+// 버블 정렬
 for (let i = 0; i < n; i++) {
-  let idx = i;
-  for (let j = i + 1; j < n; j++) {
-    if (arr[idx] > arr[j]) {
-      idx = j;
+  for (let j = 0; j < n; j++) {
+    if (arr[j] > arr[j + 1]) {
+      // 오름차순 정렬
+      let tmp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = tmp;
     }
   }
-  [arr[i], arr[idx]] = [arr[idx], arr[i]];
 }
 
-console.log(arr.join("\n"));
+console.log(arr.join('\n'));

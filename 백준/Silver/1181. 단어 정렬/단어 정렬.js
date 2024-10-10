da26b87fbc275ab6e1cd2d4bff82f835a19e5c0c
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-10-10 18:57:22 +0900

    [Silver V] Title: 단어 정렬, Time: 220 ms, Memory: 19652 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,21 +1,26 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");;
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let n = Number(input[0]);
-let wordsSet = new Set();
-
+// 1181: 단어 정렬
+let n = Number(input[0]); // N개의 수
+let mySet = new Set();
 for (let i = 0; i < n; i++) {
-  // 중복 제거 
-  wordsSet.add(input[i+1].trim());
+  mySet.add(input[i + 1]);
 }
 
-let words = Array.from(wordsSet); // Set을 배열로 변환
-
-words.sort(function(a, b) {
-  if (a.length == b.length) {
-    return a.localeCompare(b);
-  }
+// 단어 정렬: 1. 길이가 짧은 것부터, 2. 길이가 같으면 사전 순으로, 3. 중복은 제거
+function compare(a, b) {
+  if (a.length == b.length) return a.localeCompare(b); // 길이가 같으면 사전 순으로
   return a.length - b.length;
-})
+}
 
-console.log(words.join("\n"));
+let arr = Array.from(mySet); // 집합을 배열로 변환
+arr.sort(compare);
+
+// 결과 문자열 합치기
+let result = "";
+for (let i = 0; i < arr.length; i++) {
+  result += arr[i] + "\n";
+}
+
+console.log(result);

e360b46faad57c556b5976b42cd4170c5add81ed
서가은 <137467879+gaxunsxo@users.noreply.github.com>
2024-11-14 13:47:02 +0900

    [Silver III] Title: N과 M (3), Time: 1228 ms, Memory: 251288 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.js
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -1,34 +1,28 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const { arrayBuffer } = require("stream/consumers");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-// 15651: N과 M (3)
+// 15651번. N과 M(3)
 let [n, m] = input[0].split(" ").map(Number);
 let arr = [];
-for (let i = 1; i <= n; i++) {
-  arr.push(i);
-}
-let selected = []; // 현재 조합에 포함된 원소(element)의 인덱스
+for (let i = 1; i <= n; i++) arr.push(i);
 
-// 1부터 n까지의 자연수 중에 m개(중복 가능)를 고른 수열 
-// 고른 수열은 사전 순으로 출력 
-let answer = "";
-function dfs(arr, depth) {
+let selected = [];
+let results = [];
+
+// 1부터 N까지 자연수 중에서 M개를 고른 수열 (중복 가능)
+function dfs(start, depth) {
   if (depth == m) {
-    // 고른 수열을 모두 출력하기 
-    let result = []; // 조합(combination) 결과 저장 테이블
-    for (let i of selected) 
-      result.push(arr[i]);
-    for (let x of result) 
-      answer += x + " "; // 계산된 조합을 실질적으로 처리하는 부분
-    answer += "\n"
+    results.push([...selected]);
     return;
   }
 
-  for (let i = 0; i < arr.length; i++) {
-    selected.push(i); 
-    dfs(arr, depth + 1, i + 1); // 조합이므로, 다음 원소를 넣어 재귀 호출
+  for (let i = 1; i <= n; i++) {
+    selected.push(i);
+    dfs(start + 1, depth + 1);
     selected.pop();
   }
 }
-dfs(arr, 0);
-console.log(answer);
+
+dfs(1, 0);
+console.log(results.map((selected) => selected.join(" ")).join("\n"));
